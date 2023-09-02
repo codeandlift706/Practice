373ee4c2a8af9d4b3e0e182b3492437dec243932
Code Queen <channguyen97@gmail.com>
2023-09-02 11:26:30 -0700

    added pseudocode for javascript

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
-//global variables -globally ac
+//global variables -globally accessible
+/*
+card - that'll hold the date, temp, wind, humidity
+empty card container for selected city
+empty card container for 5 day
+empty list container for generated cities
 
+
+
+
+*/
 //functions - tell your sibling what the items on the page do
+/*
 
+for when you input text in the search/form bar
+    when you hit search
+    the searched for cities will appear underneath as a list element
+
+
+
+for when you
+
+
+
+*/
 //processes - how the items on the page will execute
+/*
+
+
+
+
+
+
+*/
