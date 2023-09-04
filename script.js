93cb9a4c4505b8ff4121ea735626da02ea08ac8b
Code Queen <channguyen97@gmail.com>
2023-09-04 16:23:19 -0700

    search function complete

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 //Global Variables -globally accessible
+const searchButton = document.querySelector(".searchBtn");
+
+const searchedCitiesContainer = document.querySelector(".searchedCitiesContainer");
+//WE WILL CREATE A LIST EL AND APPEND TO THIS
+
+const currentCardContainer = document.querySelector(".currentCardContainer");
+//WE WILL CREATE A CARD EL AND APPEND TO THIS
+
+const fiveCardContainer = document.querySelector(".fiveCardContainer");
+//WE WILL CREATE A CARD EL AND APPEND TO THIS
+
+const cityInformationContainer = document.querySelector(".cityInformationContainer");
+//WE WILL CREATE AND APPEND LIST EL TO THIS - DISPLAY TEMP, HUMIDITY, ETC
 /*
+
+
+
+
 card - that'll hold the date, temp, wind, humidity
 empty card container for selected city, current day
 empty card container for 5 day
@@ -8,14 +25,13 @@ base URL: https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&a
 var APIKey = "1df7696f823ad8cc7efbb5f9a31ff2b8"
 https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-var searchButton = querySelector("#searchBtn");
 
 fetch(queryURL)
 Remember that the query URL won't work automatically as it's written. You'll need to adjust your application to accept user input, to store in the city variable that you've created.
 
 -----------------------------------------------------------------------------------------------
 */
-//Functions - tell your sibling what the items on the page do
+//Functions
 /*
 
 We need an observe button clicks behavior
@@ -26,23 +42,46 @@ We need an observe button clicks behavior
     attach attribute
     append the li to the empty list container
 
+*/
+function searchCity (event) {
+    event.preventDefault();
 
-function searchCity () {
+    const inputField = document.querySelector("#searchCardInput");
+    const userInput = inputField.value.trim();
 
+    if (userInput === "") {
+        alert("You must enter a city to perform a search.");
+        
+        return;
+    }
+    
+    inputField.value = "";
+
+
+    fetch() //request URL
+    .then(function(response) { //server response
+    return response.json(); //what we get here, we now are going to call it data at line 66
+
+    })
+    .then(function(data) {
+        console.log(data);
+    })
 }
 
 
+
+/*
 We need to have the list item click handler function
     we'll need to give this function the data from the fetch/API call
     when you click on a list item that populated
     pass this info to the page url as query string
+*/
 
-
-function listCity () {
+function listCity (event) {
 
 }
 
-
+/*
 We need to have a renderDivs function
     for all queries data, create:
 
@@ -73,17 +112,19 @@ We need to have a renderDivs function
     -Humidity: __%
     display: text from data API fetch call
     append to each individual card
+*/
 
 
-
-function displayCityInfo () {
+function displayCityInfo (event) { //renderDivs function
+//need to know what the current search value is - know which element was clicked on so we can get its text content
+//update the page to reflect the currently selected value
+console.log("Your searchedCities button was clicked");
 
 }
 
-
+/*
 -----------------------------------------------------------------------------------------------
-*/
-//Processes - how the items on the page will execute
+//Processes
 /*
 
 target the search button, listen for submit to run the first function
@@ -91,18 +132,31 @@ on the click event, make API call to fetch data from the city
 so that the city/geographical pops up
 the previously searched for cities will be saved in the list into local storage, and then retrieved from local storage to stay in display on the list
 
-searchButton.addEventListener("submit", event) {
+*/
+searchButton.addEventListener("click", searchCity);
 
-}
 
-
+/*
 target the listitems in the results div, listen for submit to run the second function
 fetch the data related to the selected city, date, temp, wind, humidity
 on the click event from the selected city, clear out the previous city's displayed data, show the new city
 run the renderDivs function
-
-
-
-
-
 */
+
+searchedCitiesContainer.addEventListener("click", displayCityInfo);
+
+
+
+
+
+
+// plug in bootstrap later
+/*
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
+    crossorigin="anonymous"></script>
+*/
+
