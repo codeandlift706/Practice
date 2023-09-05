c69bc46c3620e49e70cccd5024deb74c62c2e480
Code Queen <channguyen97@gmail.com>
2023-09-04 21:06:24 -0700

    added display cito info function, cleaned up pseudocode

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Global Variables -globally accessible
+//Global Variables
 const searchButton = document.querySelector(".searchBtn");
 
 const searchedCitiesContainer = document.querySelector(".searchedCitiesContainer");
@@ -13,19 +13,8 @@ const fiveCardContainer = document.querySelector(".fiveCardContainer");
 const cityInformationContainer = document.querySelector(".cityInformationContainer");
 //WE WILL CREATE AND APPEND LIST EL TO THIS - DISPLAY DATE, TEMP, WIND, HUMIDITY
 
-
-
-//THIS IS 5 DAY FORECAST - REQUEST BY CITY NAME
-
+//API INFORMATION
 /*
-base URL: https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-var APIKey = "1df7696f823ad8cc7efbb5f9a31ff2b8"
-https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-
-fetch(queryURL)
-Remember that the query URL won't work automatically as it's written. You'll need to adjust your application to accept user input, to store in the city variable that you've created.
-
 For city:
 city.name
 
@@ -43,28 +32,16 @@ list.main.humidity
 For symbol=
 list.weather.icon
 
-
 const temperatureDisplay = data[i].list.main.temp;
-            const windDisplay = data[i].list.wind.speed;
-            const humidityDisplay = data[i].list.main.humidity;
-            const symbolDisplay = data[i].list.weather.icon;
-
-
------------------------------------------------------------------------------------------------
+const windDisplay = data[i].list.wind.speed;
+const humidityDisplay = data[i].list.main.humidity;
+const symbolDisplay = data[i].list.weather.icon;
 */
+
+//---------------------------------------------------------------------------------------------
 //Functions
-/*
 
-We need an observe button clicks behavior
-    when you input text in the form bar-captures the input field value
-    plug our input field value into a fetch call-dynamically display results-the searched city name will appear underneath as a list element
-
-    create li element
-    attach attribute
-    append the li to the empty list container
-
-*/
-
+//function for observe button clicks behavior --when you input text in the form bar-captures the input field value
 function searchCity(event) {
     event.preventDefault();
 
@@ -77,39 +54,48 @@ function searchCity(event) {
         return;
     }
 
-inputField.value = "";
+    inputField.value = "";
 
     listCityName(userInput); //we are going to supply userInput
 }
 
 
-/*
-We need to have the list item click handler function
-    we'll need to give this function the data from the fetch/API call
-    when you click on a list item that populated
-    pass this info to the page url as query string
-*/
-
+//function to fetch API data
 function listCityName(userInput) { //userInput was not defined in this function's scope but we need it. We are telling it to expect userInput
     fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${userInput}&appid=1df7696f823ad8cc7efbb5f9a31ff2b8`) //anything wrapped in ${} is a variable
-    .then(function (response) { //server response
-        return response.json(); //what we get here, we now are going to call it data at line 66
-    })
-    .then(function (data) {
-        console.log(data);
+        .then(function (response) { //server response
+            return response.json(); //what we get here, we now are going to call it data at line 66
+        })
+        .then(function (data) {
+            console.log(data);
 
-        
-        for (let i = 0; i < data.length; i++) {
-            displayCityInfo(data[i]); //we need to tell the function what is each data object, each data object occupies an index of the data array. This tells the function what argument we're passing
-        }
 
-    })
+            for (let i = 0; i < data.length; i++) {
+                displayCityName(data[i]); //we need to tell the function what is each data object, each data object occupies an index of the data array. This tells the function what argument we're passing
+            }
+
+        })
+}
+
+
+//function to display the city name as a list element
+function displayCityName (currentObject) { //renderDivs function, we're aliasing the previous data point as currentObject. It does not have to be called the same value it is passed as. It is essentially var currentObject = data[i]. We tell this function to catch that current object before it will be logged
+
+    const cityName = currentObject.city.name;
+
+    console.log(cityName); // WHY ISN'T THIS SHOWING UP???????
+
+    //create list item, click event on list item?
+    // searchedCitiesContainer div
+    const cityList = document.createElement("li");
+
+    cityList.textContent = `${cityName}`;
+    searchedCitiesContainer.appendChild(cityList);
+    displayCityInfo(cityList);
 }
 
 /*
-We need to have a renderDivs function
-    for all queries data, create:
-
+function to display the city weather details
     if current day,
     create current day card element
     attach attr so that this shows on single line
@@ -138,13 +124,17 @@ We need to have a renderDivs function
     display: text from data API fetch call
     append to each individual card
 */
+function displayCityInfo (cityList) { // renderDivs function
 
+    const tempDisplay = list.list.main.temp;
+    const windDisplay = cityList.list.wind.speed;
+    const humidityDisplay = cityList.list.main.humidity;
+    const symbolDisplay = cityList.list.weather.icon;
 
-function displayCityInfo(currentObject) { //renderDivs function, we're aliasing the previous data point as currentObject. It does not have to be called the same value it is passed as. It is essentially var currentObject = data[i]. We tell this function to catch that current object before it will be logged
-    
-    const cityNameDisplay = currentObject.city.name; // TODO loop this
+    const cityFullInfo = document.createElement("li");
 
-    console.log(cityNameDisplay);
+    cityFullInfo.textContent = `${cityName}`, `${tempDisplay}`, `${windDisplay}`, `${humidityDisplay}`,`${symbolDisplay}`;
+    cityInformationContainer.appendChild(cityFullInfo);
 }
 
 /*
@@ -169,7 +159,6 @@ run the renderDivs function
 */
 
 searchedCitiesContainer.addEventListener("click", displayCityInfo);
-
 
 
 
