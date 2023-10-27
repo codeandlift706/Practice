264ea1764eaa6e991d26ead627a67af0a4777dcb
Code Queen <channguyen97@gmail.com>
2023-10-26 19:07:55 -0700

    cleaned code

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,7 +23,7 @@ function searchCity(event) {
     inputField.value = "";
 
     const cityButtonEl = document.createElement("button"); //create a button for each city searched
-    cityButtonEl.setAttribute('class',"btn btn-outline-dark");
+    cityButtonEl.setAttribute('class', "btn btn-outline-dark");
     cityButtonEl.textContent = userInput;
 
     cityButtonEl.addEventListener("click", function (event) { //attach event listener for the city button
@@ -41,12 +41,11 @@ function searchCity(event) {
 
 //function to fetch API data for current weather
 function getCurrentDay(userInput) {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=1df7696f823ad8cc7efbb5f9a31ff2b8&units=imperial`) //anything wrapped in ${} is a variable
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=1df7696f823ad8cc7efbb5f9a31ff2b8&units=imperial`)
         .then(function (response) { //server response
-            return response.json(); //what we get here, we now are going to call it data at line 66
+            return response.json();
         })
         .then(function (data) {
-            // console.log(data);
             displayCurrentDay(data);
         })
 }
@@ -54,16 +53,15 @@ function getCurrentDay(userInput) {
 
 //function to fetch API data for 5-day forecast
 function getFiveDay(userInput) {
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${userInput}&appid=1df7696f823ad8cc7efbb5f9a31ff2b8&units=imperial`) //anything wrapped in ${} is a variable
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${userInput}&appid=1df7696f823ad8cc7efbb5f9a31ff2b8&units=imperial`)
         .then(function (response) { //server response
-            return response.json(); //what we get here, we now are going to call it data at line 66
+            return response.json();
         })
         .then(function (data) {
             // console.log(data);
             const filteredArray = []
             for (let i = 0; i < data.list.length; i++) {
                 if (data.list[i].dt_txt.includes("00:00:00")) {
-                    // console.log(data.list[i]);
                     filteredArray.push(data.list[i]);
                 }
             }
@@ -77,9 +75,6 @@ function displayCurrentDay(currentDayObject) {
 
     currentCardContainer.innerHTML = "";
     const cityName = currentDayObject.name;
-    // console.log(cityName);
-    // console.log(filteredArray);
-console.log(currentDayObject);
 
     const temp = currentDayObject.main.temp;
     const wind = currentDayObject.wind.speed;
@@ -93,20 +88,19 @@ console.log(currentDayObject);
     const year = now.getFullYear();
     const newDateFormat = `${month}/${day}/${year}`;
 
-    
+
     const currentCardEl = document.createElement("card"); //create a card for entry
-    
+
     let cityHeader = document.createElement('h1') //create h2 element to append city name
     cityHeader.textContent = `${cityName} ${newDateFormat}`;
     currentCardEl.appendChild(cityHeader);
-    
+
     let weatherIcon = document.createElement('img'); //create img element
     weatherIcon.src = `https://openweathermap.org/img/wn/${icon}.png`; // set img src = URL
     currentCardEl.appendChild(weatherIcon);
-    
+
     const currentWeatherCard = (
-        `
-        Temp: ${temp} °F
+        `Temp: ${temp} °F
         Wind: ${wind} MPH
         Humidity: ${humidity}%`
     ); //create a variable weatherCard to show this info
@@ -120,7 +114,6 @@ console.log(currentDayObject);
 
     currentCardContainer.append(currentCardEl); //append to currentCardContainer
     currentCardEl.append(...currentWeatherCardP); //append each p element to the card element
-
 
     const currentDayData = { //create an object with these properties so we can store to local storage
         city: cityName,
@@ -158,25 +151,23 @@ function displayFiveDay(fiveDayObject, filteredArray) {
         const newDateFormat = `${month}/${day}/${year}`;
 
         const cardEl = document.createElement("card"); //create a card for each entry
-        let dateHeader = document.createElement('h4') //create h2 element to append city name
+        let dateHeader = document.createElement('h4') //create h element to append city name
         dateHeader.textContent = `${newDateFormat}`;
         cardEl.appendChild(dateHeader);
 
         const weatherCard = (
-            `
-                Temp: ${temp}°F
-                Wind: ${wind} MPH
-                Humidity: ${humidity}%
-                `
+            `Temp: ${temp}°F
+            Wind: ${wind} MPH
+            Humidity: ${humidity}%`
         ); //for every entry in filteredArray, create a variable weatherCard to show this info
 
         const weatherCardLines = weatherCard.split('\n'); //split the weatherCard string into an array of strings using '\n' to create a new line
-        const weatherCardP = weatherCardLines.map(line => document.createElement('h5')); //create a new p element for each string in the array, map through each string
+        const weatherCardP = weatherCardLines.map(line => document.createElement('h5')); //create a new h5 element for each string in the array, map through each string
 
         weatherCardP.forEach((h5, index) => {
             h5.textContent = weatherCardLines[index];
-        }); //for each string in the new array, set the p to display the text
-        
+        }); //for each string in the new array, set the h5 to display the text
+
 
         let weatherIcon = document.createElement('img'); //create img for every icon
         weatherIcon.src = `https://openweathermap.org/img/wn/${icon}.png`;
@@ -186,7 +177,7 @@ function displayFiveDay(fiveDayObject, filteredArray) {
         cardEl.append(...weatherCardP); //append each p element to the cardEl element
 
 
-//save to local storage
+        //save to local storage
         const forecastData = { //create an object with these properties so we can store to local storage
             city: cityName,
             date: newDateFormat,
